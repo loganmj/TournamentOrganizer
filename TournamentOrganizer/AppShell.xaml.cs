35c165e9f7127e55fff8b39be64005414f74c0da
loganmj <53303258+loganmj@users.noreply.github.com>
2023-10-23 08:23:10 -0500

    Add project files.

---
+++ TournamentOrganizer/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace TournamentOrganizer
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ TournamentOrganizer/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace TournamentOrganizer
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
