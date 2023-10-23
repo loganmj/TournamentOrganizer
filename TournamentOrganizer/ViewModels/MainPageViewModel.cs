9881fb15da465690472b65823da517b903e44019
loganmj <53303258+loganmj@users.noreply.github.com>
2023-10-23 10:18:03 -0500

    Added MainPageViewModel.

---
+++ TournamentOrganizer/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TournamentOrganizer.ViewModels
+{
+    /// <summary>
+    /// Controller for the MainPage view.
+    /// </summary>
+    internal class MainPageViewModel : ViewModelBase
+    {
+        #region Constructors
+
+        /// <summary>
+        /// Generic constructor.
+        /// </summary>
+        public MainPageViewModel() 
+        {
+
+        }
+
+        #endregion
+    }
+}
