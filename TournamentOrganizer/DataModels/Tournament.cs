0d7e8803fe7208ad63c79f1d7971408a4371999c
loganmj <53303258+loganmj@users.noreply.github.com>
2023-10-23 10:41:16 -0500

    Added the Tournament class.

---
+++ TournamentOrganizer/DataModels/Tournament.cs
@@ -0,0 +1,54 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TournamentOrganizer.DataModels
+{
+    /// <summary>
+    /// Represents a tournament.
+    /// </summary>
+    internal partial class Tournament : ObservableObject
+    {
+        #region Fields
+
+        [ObservableProperty]
+        private List<Participant> _participants;
+
+        #endregion
+
+        #region Constructors
+
+        /// <summary>
+        /// Generic constructor.
+        /// </summary>
+        public Tournament() { }
+
+        #region
+
+        #region Public Methods
+
+        /// <summary>
+        /// Adds a Participant to the Tournament.
+        /// </summary>
+        public void AddParticipant() { }
+
+        /// <summary>
+        /// Removes a Participant 
+        /// </summary>
+        public void RemoveParticipant() { }
+
+        /// <summary>
+        /// Returns a string representation of the Tournament.
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+
+        #endregion
+    }
+}
