cf3a629829c95b457f3c57a30249c32b42b74c87
loganmj <53303258+loganmj@users.noreply.github.com>
2023-10-23 10:29:13 -0500

    Added the Participant data class.

---
+++ TournamentOrganizer/DataModels/Participant.cs
@@ -0,0 +1,51 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TournamentOrganizer.DataModels
+{
+    /// <summary>
+    /// Represents a participant in the tournament.
+    /// </summary>
+    internal partial class Participant : ObservableObject
+    {
+        #region Fields
+
+        /// <summary>
+        /// The name of the Participant.
+        /// </summary>
+        [ObservableProperty]
+        private string _name;
+
+        #endregion
+
+        #region Constructors
+
+        /// <summary>
+        /// Basic constructor requires a name for the participant.
+        /// </summary>
+        /// <param name="name"></param>
+        public Participant(string name) 
+        {
+            Name = name;
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        /// <summary>
+        /// Returns a string representation of the Participant.
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            return $"Participant | Name: {Name}";
+        }
+
+        #endregion
+    }
+}
