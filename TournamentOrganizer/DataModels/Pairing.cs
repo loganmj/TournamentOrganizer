5b4b48861d5db2e7fe3e130380c06b3c15ba9d20
loganmj <53303258+loganmj@users.noreply.github.com>
2023-10-23 10:58:52 -0500

    Added the Pairing class.

---
+++ TournamentOrganizer/DataModels/Pairing.cs
@@ -0,0 +1,48 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace TournamentOrganizer.DataModels
+{
+    /// <summary>
+    /// Represents a pairing between two Participants.
+    /// </summary>
+    internal partial class Pairing : ObservableObject
+    {
+        #region Fields
+
+        [ObservableProperty]
+        private Participant _participant1;
+
+        [ObservableProperty]
+        private Participant _participant2;
+
+        #endregion
+
+        #region Constructors
+
+        /// <summary>
+        /// Basic constructor requires two Participants.
+        /// </summary>
+        /// <param name="participant1"></param>
+        /// <param name="participant2"></param>
+        public Pairing(Participant participant1, Participant participant2)
+        {
+            Participant1 = participant1;
+            Participant2 = participant2;
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        /// <summary>
+        /// Returns a string representation of the Pairing.
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+
+        #endregion
+    }
+}
