a285383d148b79aa4ae7cbe4af8d2c4e7ad05608
loganmj <53303258+loganmj@users.noreply.github.com>
2023-10-23 09:41:01 -0500

    Added ViewModelBase.

---
+++ TournamentOrganizer/ViewModels/ViewModelBase.cs
@@ -0,0 +1,25 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TournamentOrganizer.ViewModels
+{
+    /// <summary>
+    /// A base class for ViewModel objects.
+    /// </summary>
+    internal partial class ViewModelBase : ObservableObject
+    {
+        #region Fields
+
+        [ObservableProperty]
+        private bool _isBusy;
+
+        [ObservableProperty]
+        private string _title;
+
+        #endregion
+    }
+}
