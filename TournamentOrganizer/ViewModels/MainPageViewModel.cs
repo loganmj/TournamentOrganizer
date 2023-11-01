b4f949fa4369b61b6cdb31a4d886208b7b945fbf
loganmj <53303258+loganmj@users.noreply.github.com>
2023-11-01 08:36:34 -0500

    Removed iOS from supported platforms.
    Connected MainPageViewModel to MainPage.

--- TournamentOrganizer/ViewModels/MainPageViewModel.cs
+++ TournamentOrganizer/ViewModels/MainPageViewModel.cs
@@ -5,14 +5,12 @@ namespace TournamentOrganizer.ViewModels
 {
     /// <summary>
     /// Controller for the MainPage view.
+    /// The MainPage should have options to select a Tournament type,
+    /// and to start a Tournament.
     /// </summary>
     internal partial class MainPageViewModel : ViewModelBase
     {
         #region Fields
-
-        [ObservableProperty]
-        private ITournament _tournament;
-
         #endregion
 
         #region Constructors
@@ -22,8 +20,7 @@ namespace TournamentOrganizer.ViewModels
         /// </summary>
         public MainPageViewModel()
         {
-            // TEST
-            _tournament = TournamentFactory.CreateTournament(ITournament.TournamentTypes.RoundRobin);
+            Title = "Tournament Organizer";
         }
 
         #endregion
